7b6b037d2943ea3ed15a1dbb3ba2d2c143399376
Martin Kapp <martin@coderats.net>
2016-08-03 23:20:36 +0300

    Fix license, and add required engine version to package.json

--- install.js
+++ install.js
@@ -1,6 +1,4 @@
-var Validate = require('git-validate'),
-	utils = require('git-validate/lib/utils'),
-	path = require('path');
+var Validate = require('git-validate');
 
 /* Copy config files for editors */
 Validate.copy('files/eslintrc.json', '.eslintrc.json');
@@ -8,7 +6,7 @@ Validate.copy('files/eslintignore', '.eslintignore');
 Validate.copy('files/editorconfig', '.editorconfig');
 
 /* Install scripts */
-Validate.installScript('lint', path.relative(utils.findProjectRoot(), 'node_modules/eslint/bin/eslint.js') + ' .');
+Validate.installScript('lint', 'node_modules/eslint/bin/eslint.js .');
 
 /* Install git hooks */
 Validate.configureHook('pre-commit', ['lint', 'test']);
