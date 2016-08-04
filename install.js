98a355b1f381b5c0a734cc61cf1366860ef1993c
Martin Kapp <martin@coderats.net>
2016-08-04 22:36:05 +0300

    Add overwrite flag to lint and editor configs

--- install.js
+++ install.js
@@ -1,9 +1,9 @@
 var Validate = require('git-validate');
 
 /* Copy config files for editors */
-Validate.copy('files/eslintrc.json', '.eslintrc.json');
+Validate.copy('files/eslintrc.json', '.eslintrc.json', {overwrite: true});
 Validate.copy('files/eslintignore', '.eslintignore');
-Validate.copy('files/editorconfig', '.editorconfig');
+Validate.copy('files/editorconfig', '.editorconfig', {overwrite: true});
 
 /* Install scripts */
 Validate.installScript('lint', 'node_modules/eslint/bin/eslint.js .');
